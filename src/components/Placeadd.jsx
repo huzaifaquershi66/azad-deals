42f02d8dc1c6e977ee51350f066e11e1eb717407
huzaifa56jbh <hadrish153@gmail.com>
2024-11-13 06:27:33 +0500

    d

--- src/components/Placeadd.jsx
+++ src/components/Placeadd.jsx
@@ -1255,7 +1255,7 @@ const handleCityClick = (city) => {
 
     {/* Right Side: Live Preview */}
    
-    <div className="h-[650px] relative mt-4   w-full md:max-w-[500px] bg-gradient-to-r from-gray-600 to-gray-900 p-6 sm:p-8 rounded-2xl shadow-3xl border border-gray-300 transform transition-transform duration-300 ease-out hover:shadow-2xl md:mt-20 hover:border-gray-500 md:mx-4 lg:mx-52">
+    <div className="h-[650px] relative mt-4   w-full md:max-w-[500px] bg-gradient-to-r from-gray-600 to-gray-900 p-6 sm:p-8 rounded-2xl shadow-3xl border border-gray-300 transform transition-transform duration-300 ease-out hover:shadow-2xl md:mt-20 hover:border-gray-500 md:mx-4 lg:mx-auto">
   {/* Live Preview Badge with Pulse Effect */}
   <span className="absolute top-2 left-1/2 transform -translate-x-1/2 font-helveticaLight text-white font-bold bg-gradient-to-r from-orange-500 to-purple-700 rounded-full px-4 py-2 shadow-lg z-20 transition-transform duration-200 hover:scale-110 animate-pulse">
     Live Preview
